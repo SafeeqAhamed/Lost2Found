ee1f819d15373becb55182e4b339f151ad0bd492
SafeeqAhamed <ssafeeq2004@gmail.com>
2026-08-10 02:44:50 +0530

    Fix login security

--- backend/src/main/java/com/example/lostfound/model/User.java
+++ backend/src/main/java/com/example/lostfound/model/User.java
@@ -1,18 +1,55 @@
 package com.example.lostfound.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.NoArgsConstructor;
 
 @Data
-@Document(collection="users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String username;
     private String email;
     private String password;
-}
 
+    // Explicit getters and setters if Lombok is not processing in the Docker container
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
